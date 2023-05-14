fc16f53fb1b86105d4da97eaccf26f44dd5c2b00
Yashgupta9330 <121821372+Yashgupta9330@users.noreply.github.com>
2023-05-14 18:14:28 +0530

    Add files via upload

--- weather.c
+++ weather.c
@@ -1,108 +1,60 @@
-
 #include <stdio.h>
-/*#include <curl/curl.h>
-//#include <jansson.h>
-
-int main(void) {
-    CURL *curl;
-    CURLcode res;
-    char url[100];
-    char *json_string;
-    json_t *root;
-    json_error_t error;
-
-    // Set the API URL with your API key and the location you want to get the weather data for
-    sprintf(url, "http://api.openweathermap.org/data/2.5/weather?q=New+York&appid=3f92114c01a3d0f4c9962e0f1004278a");
-
-    // Initialize cURL
-    curl = curl_easy_init();
-
-    if (curl) {
-        // Set the URL and options for cURL
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
-
-        // Retrieve the data from the API and store it in a string
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &json_string);
-        res = curl_easy_perform(curl);
-
-        // Check if the request was successful
-        if (res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-            return 1;
-        }
-
-        // Parse the JSON data using Jansson
-        root = json_loads(json_string, 0, &error);
-
-        if (!root) {
-            fprintf(stderr, "json_loads() failed: %s\n", error.text);
-            return 1;
-        }
-
-        // Extract the relevant weather data from the JSON object
-        json_t *main = json_object_get(root, "main");
-        json_t *temp = json_object_get(main, "temp");
-        double temperature = json_real_value(temp) - 273.15; // Convert from Kelvin to Celsius
-        json_t *weather = json_object_get(root, "weather");
-        json_t *description = json_object_get(json_array_get(weather, 0), "description");
-        const char *description_str = json_string_value(description);
-
-        // Print out the weather data in a user-friendly format
-        printf("Current temperature in New York: %.1f°C\n", temperature);
-        printf("Current weather conditions: %s\n", description_str);
-
-        // Clean up
-        curl_easy_cleanup(curl);
-        json_decref(root);
-        free(json_string);
-    }
-
-    return 0;
+#include <stdlib.h>
+#include <curl/curl.h>
+#include <cJSON.h>
+// Function to handle CURL write callback
+size_t write_callback(char *data, size_t size, size_t nmemb, char *buffer) {
+size_t total_size = size * nmemb;
+buffer = realloc(buffer, total_size + 1);
+if(buffer == NULL) {
+printf("Error: Unable to allocate memory.\n");
+return 0;
 }
-
-// Callback function for cURL to write the API response to a string
-static size_t write_callback(char *ptr, size_t size, size_t nmemb, char **userdata) {
-    size_t realsize = size * nmemb;
-
-    *userdata = realloc(*userdata, strlen(*userdata) + realsize + 1);
-    if (*userdata == NULL) {
-        fprintf(stderr "realloc() failed\n");
-    return 0;
+strncat(buffer, data, total_size);
+return total_size;
 }
-
-memcpy(*userdata + strlen(*userdata), ptr, realsize);
-*(*userdata + strlen(*userdata) + realsize) = '\0';
-
-return realsize;
-} */
-
-/*vbnet
-Copy code
-
-This program retrieves weather data for New York from the OpenWeatherMap API, extracts the temperature and weather description from the JSON data, and displays them in a user-friendly format. You will need to replace "YOUR_API_KEY" in the URL with your actual API key.
-
-4. Compile and run the code: Compile the program using the following command:
-
-gcc -o weather weather.c -lcurl -ljansson
-
-arduino
-Copy code
-
-This will link the cURL and Jansson libraries with the program. Then, run the program using the following command:
-
-./weather
-
-vbnet
-Copy code
-
-You should see the current temperature and weather conditions for New York displayed in the terminal.
-
-Note: This program is just a basic example and can be modified to retrieve weather data for other locations and display additional information. Also, make sure to handle errors and edge cases appropriately.
-
-
-
-
-Regenerate response */
+int main() {
+CURL *curl;
+CURLcode res;
+char *weather_api_url = "http://api.openweathermap.org/data/2.5/weather?q=New+York&appid=YOUR_API_KEY";
+char *api_key = "3f92114c01a3d0f4c9962e0f1004278a";
+char *buffer = malloc(4096 * sizeof(char));
+if(buffer == NULL) {
+printf("Error: Unable to allocate memory.\n");
+return 1;
+}
+buffer[0] = '\0';
+curl = curl_easy_init();
+if(curl) {
+char request_url[512];
+sprintf(request_url, "%s?apikey=%s", weather_api_url, api_key);
+curl_easy_setopt(curl, CURLOPT_URL, request_url);
+curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+curl_easy_setopt(curl, CURLOPT_WRITEDATA, buffer);
+res = curl_easy_perform(curl);
+if(res != CURLE_OK) {
+printf("Error: %s\n", curl_easy_strerror(res));
+return 1;
+}
+curl_easy_cleanup(curl);
+// Parse JSON response
+cJSON *json = cJSON_Parse(buffer);
+if(json == NULL) {
+printf("Error: Failed to parsse JSON.\n");
+free(buffer);
+return 1;
+}
+// Extract desired weather data from JSON
+cJSON *temperature = cJSON_GetObjectItem(json, "temperature");
+cJSON *humidity = cJSON_GetObjectItem(json, "humidity");
+cJSON *description = cJSON_GetObjectItem(json, "description");
+// Display weather data
+printf("Temperature: %.2fÃ‚Â°C\n", temperature->valuedouble);
+printf("Humidity: %.2f%%\n", humidity->valuedouble);
+printf("Description: %s\n", description->valuestring);
+// Clean up
+cJSON_Delete(json);
+free(buffer);
+}
+return 0;
+}
